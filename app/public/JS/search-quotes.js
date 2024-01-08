6792000ed9188823409ef26d82b2618780abeb92
gabrioviski <gabrielcarvalhos.snt@gmail.com>
2024-01-08 00:58:38 -0300

    css do input autocomplete aprimorado

--- app/public/JS/search-quotes.js
+++ app/public/JS/search-quotes.js
@@ -2,7 +2,6 @@ const quotes = document.querySelectorAll('.suggestions-options a')
 const search = document.querySelector('.search-bar input')
 
 search.addEventListener('focus', () => {
-    // console.log(1);
     search.classList.add('border')
     if (!search.value) {
         quotes.forEach(quote => quote.classList.add('show'))
@@ -10,13 +9,11 @@ search.addEventListener('focus', () => {
 })
 
 search.addEventListener('input', () => {
-    // search.classList.add('border')
     const searchText = search.value.toLowerCase()
     quotes.forEach(quote => quote.classList.remove('show'));
 
-    [...quotes].filter(quote => quote.textContent.toLowerCase().includes(searchText)).forEach(item => {
-        item.classList.add('show')
-    })
+    [...quotes].filter(quote => quote.textContent.toLowerCase().includes(searchText))
+               .forEach(quote => quote.classList.add('show'))
 })
 
 document.addEventListener('click', (e) => {
