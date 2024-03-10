c2d9d73b0ed1c945c5453d2d1d2955941245cdde
Alex Farias Vaz dos Santos <alexfarias08032006@gmail.com>
2024-03-09 21:04:35 -0300

    colancando as conexao do router

--- app/routes/main.js
+++ app/routes/main.js
@@ -1,8 +1,8 @@
 var express = require("express")
 var router = express.Router()
 
-/* var fabricaDeConexao = require("../../config/connection-factory")
-var conexao = fabricaDeConexao() */
+var fabricaDeConexao = require("../../config/connection-factory")
+var conexao = fabricaDeConexao()
 
 router.get("/", function (req, res) {
   res.render("pages/index")
