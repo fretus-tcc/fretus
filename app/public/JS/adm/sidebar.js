a2bc3bf44818e8af0da8ba9aa02c71076a7ee105
gabriel-carvalhos <gabrielcarvalhos.snt@gmail.com>
2024-04-20 19:38:24 -0300

    ajuste responsividade sidebar adm

--- app/public/JS/adm/sidebar.js
+++ app/public/JS/adm/sidebar.js
@@ -1,6 +1,12 @@
 const nav = document.querySelector('nav')
 const resizeButton = document.querySelector('.footer > .row:nth-child(2)')
+const showBtn = document.querySelector('#show')
+const sidebarMenu = document.querySelector('.sidebar')
 
 resizeButton.addEventListener('click', () => {
     nav.classList.toggle('resized')
+})
+
+showBtn.addEventListener('click', () => {
+    sidebarMenu.classList.toggle('active')
 })
