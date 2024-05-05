5c206c87aa242233681f332340cdc56162d3c90f
gabriel-carvalhos <gabrielcarvalhos.snt@gmail.com>
2024-05-05 18:27:18 -0300

    add js panel status

---
+++ app/public/JS/shipper/panel.js
@@ -0,0 +1,127 @@
+const accessToken = 'pk.eyJ1IjoiZ2FicmllbGNhcnZhbGgwIiwiYSI6ImNscG14ZDB6OTAwc3Eya29pM2dvZm5uamYifQ.IPac1tcfJTcmQLrrn937wQ'
+let route
+
+const getUserPosition = () => {
+    return new Promise((resolve, reject) => {
+        navigator.geolocation.getCurrentPosition(resolve, reject, { enableHighAccuracy: true })
+    })
+}
+
+const setUserCoords = async () => {
+    try {
+        const { coords: { latitude, longitude } } = await getUserPosition()
+        return [longitude, latitude]
+    } catch (err) {
+        return [-46.625290, -23.533773]
+    }
+}
+
+const coords = await setUserCoords()
+
+mapboxgl.accessToken = accessToken
+const map = new mapboxgl.Map({
+    container: 'map',
+    style: 'mapbox://styles/mapbox/streets-v12',
+    center: coords,
+    zoom: 12,
+    minZoom: 5,
+    language: 'pt'
+})
+map.addControl(new mapboxgl.NavigationControl())
+
+const start = new mapboxgl.Marker({
+    color: 'rgb(15, 118, 110)',
+}).setLngLat(coords)
+
+const end = new mapboxgl.Marker({
+    color: 'rgb(15, 118, 110)',
+})
+
+const ships = document.querySelectorAll('.ships')
+const form = document.querySelector('.form')
+const mapContainer = document.querySelector('.map')
+const backForm = document.querySelector('.return-form')
+
+ships.forEach(item => {
+    item.addEventListener('click', () => {
+        document.querySelector('.ships.active')?.classList.remove('active')
+        item.classList.add('active')
+
+        form.classList.add('mobile')
+        mapContainer.classList.add('mobile')
+        backForm.classList.add('show')
+        
+        const shipCoords = [item.dataset.lng, item.dataset.lat]
+        start.addTo(map)
+        end.setLngLat(shipCoords).addTo(map)
+
+        map.easeTo({
+            center: shipCoords
+        })
+
+        getRoute(start.getLngLat(), end.getLngLat())
+    })
+})
+
+const getRoute = async (start, end) => {
+    const url = `https://api.mapbox.com/directions/v5/mapbox/driving/${start.lng},${start.lat};${end.lng},${end.lat}?geometries=geojson&access_token=${accessToken}`
+    const data = await fetch(url)
+    const json = await data.json()
+    route = json.routes[0]
+
+    const geojson = {
+        type: 'Feature',
+        properties: {},
+        geometry: {
+            type: 'LineString',
+            coordinates: route.geometry.coordinates
+        }
+    }
+
+    if (map.getSource('route')) {
+        map.getSource('route').setData(geojson)
+    } else {
+        map.addLayer({
+            id: 'route',
+            type: 'line',
+            source: {
+                type: 'geojson',
+                data: geojson
+            },
+            layout: {
+                'line-join': 'round',
+                'line-cap': 'round'
+            },
+            paint: {
+                'line-color': '#3887be',
+                'line-width': 5,
+                'line-opacity': 0.75
+            }
+        })
+    }
+
+    setZoomRoute(route)
+}
+
+const setZoomRoute = route => {
+    const bounds = new mapboxgl.LngLatBounds(
+        route.geometry.coordinates[0],
+        route.geometry.coordinates[0]
+    )
+    
+    route.geometry.coordinates.forEach(coord => {
+        bounds.extend(coord)
+    })
+
+    map.fitBounds(bounds, { padding: 300 })
+}
+
+window.addEventListener('resize', () => {
+    if (route) {
+        setZoomRoute(route)
+    }
+})
+
+backForm.addEventListener('click', () => {
+    form.classList.toggle('mobile')
+})
