2d6ad46ed82bae1b6cfcd8ce0fb8a495a4f3eb65
Alex Farias Vaz dos Santos <alexfarias08032006@gmail.com>
2024-05-10 09:39:05 -0300

    Ajuste css páginação

--- app/models/admCadastroModel.js
+++ app/models/admCadastroModel.js
@@ -2,14 +2,14 @@ const pool = require('../../config/connection-factory')
 
 const admCadastroModel = {
     //Pegando usuario da tabela 
-    findByType: async (type) => {
+  findByType: async (type) => {
         try {
             const [result] = await pool.query('SELECT `id_usuario`, `nome_usuario`, `email_usuario`, `cpf_usuario`,`tipo_usuario` FROM usuario WHERE `tipo_usuario` = ?', [type])
             return result
         } catch (error) {
             return error
         }
-    },
+    }, 
     findByUserId: async (userId) => {
         try {
             const [result] = await pool.query('SELECT * FROM usuario WHERE `id_usuario` = ? ', [userId]);
