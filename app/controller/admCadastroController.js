74bd708ec811f0728433d5a785b837b8df139af1
Alex Farias Vaz dos Santos <alexfarias08032006@gmail.com>
2024-05-16 11:40:29 -0300

    ajuste editar

--- app/controller/admCadastroController.js
+++ app/controller/admCadastroController.js
@@ -1,5 +1,6 @@
 const admCadastroModel = require('../models/admCadastroModel')
-const { notifyMessages } = require('../util/Funcao')
+const { notifyMessages, validaCPF } = require('../util/Funcao')
+const tarefasModel = require("../models/cadastroModel");
 
 const { body, validationResult } = require("express-validator");
 
@@ -94,9 +95,63 @@ const admCadastroController = {
 
     },
     regrasValidacao: [
-        body("nome")
-            .isLength({ min: 3, max: 45 })
-            .withMessage("Nome invalido "),
+        body("nasc")
+            .isLength({ min: 10 })
+            .withMessage('Data inválida ')
+            .toDate()
+            .withMessage('Data inválida '),
+        
+        body("tel")
+            .isLength({ min: 15 })
+            .withMessage('Telefone incompleto ')
+            .bail()
+            .isMobilePhone()
+            .withMessage('Telefone inválido ')
+            .bail()
+            .custom(async (value) => {
+                const tel = await tarefasModel.findByTel(value)
+                if (tel.length > 0) {
+                    throw new Error('Telefone já utilizado.');
+                }
+                return true;
+            }),
+
+        body("cpf")
+            .isLength({ min: 14, max: 14 })
+            .withMessage("Cpf inválido ")
+            .bail()
+            .custom(async (value) => {
+
+                if(validaCPF(value)){
+                    const cpf = await tarefasModel.findByCpf(value)
+                    if (cpf.length > 0) {
+                        throw new Error('Cpf já utilizado');
+                    }
+                    return true;
+                } else {
+                    throw new Error('Cpf inválido');
+                }
+
+            }),
+
+        body("email")
+            .isEmail()
+            .withMessage("Email invalido ")
+            .custom(async (value) => {
+                const email = await tarefasModel.findByEmail(value)
+                if (email.length > 0) {
+                    throw new Error('Email já utilizado.');
+                }
+                return true;
+
+            }),
+
+        body("senha")
+            .isLength({ min: 8, max: 30 })
+            .withMessage("Senha inválida, deve conter pelo menos 8 caracteres")
+            .bail()
+            .matches(/^(?=.*[a-zA-Z])(?=.*[0-9])(?=.*[!@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?])/)
+            .withMessage("Senha inválida, deve conter pelo menos 1 letra, 1 número e 1 caractere especial"),
 
     ],
     //Excluir usuário da tabela 
