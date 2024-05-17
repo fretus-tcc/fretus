7e0d056382008c892e55a532c80a2a335ac7aabd
Gabriel Carvalho <gabrielcarvalhos.snt@gmail.com>
2024-05-16 22:22:44 -0300

    ajuste validacao editar

--- app/models/cadastroModel.js
+++ app/models/cadastroModel.js
@@ -6,7 +6,6 @@ const salt = bycrypt.genSaltSync(10)
 const tarefasModel = {
     create: async (data) => {
         try {
-            console.log('model', data.nasc)
             await pool.query(
                 'INSERT INTO usuario (`nome_usuario`, `data_usuario`, `telefone_usuario`, `cpf_usuario`, `email_usuario`, `senha_usuario`, `tipo_usuario`) VALUES (?, ?, ?, ?, ?, ?, ?) ',
                 [data.nome, data.nasc, data.tel, data.cpf, data.email, bycrypt.hashSync(data.senha, salt), data.type]
@@ -16,27 +15,27 @@ const tarefasModel = {
             throw error; 
         }
     },
-    findByEmail: async (data) => {
-        try {
-            const [linhas] = await pool.query('SELECT * FROM usuario WHERE email_usuario  = ? ',  [ data ])
+    findByEmail: async (data, id) => {
+        try { 
+            const [linhas] = await pool.query(`SELECT * FROM usuario WHERE email_usuario = ? ${id ? 'AND id_usuario != ?' : ''}` , [ data, id ])
             
             return linhas;
         } catch (error) {
             return error;
         }
     },
-    findByCpf: async (data) => {
+    findByCpf: async (data, id) => {
         try {
-            const [linhas] = await pool.query('SELECT * FROM usuario WHERE cpf_usuario  = ? ',  [ data ])
+            const [linhas] = await pool.query(`SELECT * FROM usuario WHERE cpf_usuario  = ? ${id ? 'AND id_usuario != ?' : ''}`,  [ data, id ])
             
             return linhas;
         } catch (error) {
             return error;
         }
     },
-    findByTel: async (data) => {
+    findByTel: async (data, id) => {
         try {
-            const [linhas] = await pool.query('SELECT * FROM usuario WHERE telefone_usuario  = ? ',  [ data ])
+            const [linhas] = await pool.query(`SELECT * FROM usuario WHERE telefone_usuario = ? ${id ? 'AND id_usuario != ?' : ''}` ,  [ data, id ])
             
             return linhas;
         } catch (error) {
