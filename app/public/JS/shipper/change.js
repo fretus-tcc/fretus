e08b519069182a37f8ccece22694adb605ec1110
gabriel-carvalhos <gabrielcarvalhos.snt@gmail.com>
2024-06-09 18:49:49 -0300

    refatorando front perfil

--- app/public/JS/shipper/change.js
+++ app/public/JS/shipper/change.js
@@ -1,71 +1,35 @@
-const change = document.querySelectorAll('.edit-row span')
-const main = document.querySelector('main')
 const form = document.querySelector('.form-perfil')
-let changed
-let cancel
-let confirm
-let input
-let inputContainer
-let inputValue
+const change = document.querySelectorAll('.changeCall')
+const inputContainer = document.querySelectorAll('.input-container')
+const input = document.querySelectorAll('.input-container > input.input-field')
+const confirmCall = document.querySelectorAll('.input-container > .confirm')
 
-change.forEach(item => {
+change.forEach((item, i) => {
     item.addEventListener('click', () => {
-        changed = item
-        changed.style = 'display: none;'
-        inputContainer = item.previousElementSibling
-        cancel = inputContainer.children[1]
-        confirm = inputContainer.children[2]
-        console.log(inputContainer.children);
-        input = inputContainer.firstElementChild
-        inputValue = input.value
-        input.removeAttribute('readonly')
-        if (input.id == 'password') {
-            input.type = 'text'
-        }
-        inputContainer.classList.add('edit')
-        input.focus()
-        input.addEventListener('blur', (e) => {
-            e.preventDefault()
-            input.value = inputValue
-            resetInput(false)
-        })
-        input.addEventListener('keydown', isEnterPressed)
-        cancel.addEventListener('mousedown', (e) => {
-            e.preventDefault()
-            input.value = inputValue
-            resetInput(false)
-        })
-        confirm.addEventListener('click', resetInput)
+        const lastValue = input[i].value
+        change[i].style.display = 'none'
+        inputContainer[i].classList.add('edit')
+        input[i].removeAttribute('readonly')
+        input[i].focus()
+        input[i].setSelectionRange(1000,1000) // seta a posiçao do cursor ao fim
+        confirmCall[i].addEventListener('mousedown', update) // é usado o mousedown, pois ele não gera conflito com o blur
+        input[i].addEventListener('keydown', isEnterPressed)
+        input[i].addEventListener('blur', cancel.bind(null, lastValue, i))
     })
+
 })
 
-function isEnterPressed(e) {
-    if (e.key == 'Enter') resetInput()
+const update = (e) => {
+    console.log('update');
+    form.submit()
 }
 
-function resetInput(submit=true) {
-    changed.style = 'display: block;'
-    input.setAttribute('readonly', 'true')
-    if (input.id == 'password') {
-        input.type = 'password'
-    }
-    inputContainer.classList.remove('edit')
-    removeEvents()
-    if (submit) form.submit()
+const isEnterPressed = (e) => {
+    if (e.key == 'Enter') update()
 }
 
-function removeEvents() {
-    input.removeEventListener('blur', resetInput)
-    input.removeEventListener('keydown', isEnterPressed)
+const cancel = (lastValue, idx) => {
+    input[idx].value = lastValue
+    inputContainer[idx].classList.remove('edit')
+    change[idx].style.display = 'block'
 }
-
-/* function showModal(msg) {
-    if (main.childElementCount > 2) {
-        main.children[2].remove()
-    }
-    let modal = document.createElement('div')
-    modal.innerHTML = `<p>${msg}</p>
-                       <div class="loading"></div>`
-    modal.classList.add('modal', 'active')
-    main.appendChild(modal)
-} */
