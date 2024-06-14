432ad84c27d309e8e0dd42d5657947c1e8dada59
Alex Farias Vaz dos Santos <alexfarias08032006@gmail.com>
2024-06-14 14:28:59 -0300

    arrumando custom tipo e mensagem

--- app/controller/FaleConoscoController.js
+++ app/controller/FaleConoscoController.js
@@ -1,32 +1,27 @@
 const FaleConoscoModel = require("../models/FaleConoscoModel");
 const { body, validationResult } = require("express-validator");
+const { validaCPF } = require("../util/Funcao");
 
 const FaleConoscoControl = {
     MensagemFaleConosco: async (req, res) => {
 
         try {
-
             const errors = validationResult(req);
-
             if (!errors.isEmpty()) {
                 console.log(errors);
                 return res.render("pages/FaleConosco", {
                     dados: req.body,
                     listaErros: errors,
                     pagina: "FaleConosco",
-
+                    autenticado: req.session.autenticado
                 });
             }
-
             try {
                 await FaleConoscoModel.createFaleconosco(req.body);
                 res.redirect('/')
-
             } catch (error) {
                 return error;
             }
-
-
         } catch (error) {
             return error;
         }
@@ -37,7 +32,46 @@ const FaleConoscoControl = {
         body("nome")
             .isLength({ min: 3, max: 45 })
             .withMessage("Nome invalido "),
+        body("cpf")
+            .isLength({ min: 14, max: 14 })
+            .withMessage("Cpf inválido ")
+            .bail()
+            .custom(async (value) => {
+                if (validaCPF(value)) {
+                    return true;
+                } else {
+                    throw new Error('Cpf inválido');
+                }
+            }),
+        body("email")
+            .isEmail()
+            .withMessage("Email invalido "),
+        body("tipo")
+            .isLength({ min: 4, max: 10 })
+            .withMessage("tipo invalido ")
+            .bail()
+            .custom((value) => {
+                const validRoles = ["Entregador", "entregador", "Cliente", "cliente"];
+                if (validRoles.includes(value)) {
+                    return true;
+                } else {
+                    throw new Error('Por favor, preencha os campos com "Cliente" ou "Entregador".');
+                }
+            }),
 
+        body("assunto")
+            .isLength({ min: 4, max: 50 })
+            .withMessage("assunto invalido")
+        ,
+        body("mensagem")
+            .custom((value) => {
+                const wordCount = value.split(/\s+/).length;
+                if (wordCount >= 15) {
+                    return true;
+                } else {
+                    throw new Error('Mensagem inválida, mínimo 15 palavras');
+                }
+            }),
 
     ],
 
