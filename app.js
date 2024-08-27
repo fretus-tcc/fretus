1ba3fee987039921639836112a1f9083fd186f68
Alex Farias Vaz dos Santos <alexfarias08032006@gmail.com>
2024-08-27 00:34:24 -0300

    revertando oq eu fiz

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const app = express()
 const methodOverride = require('method-override')
 const session = require('express-session')
 const flash = require('connect-flash')
-const port = 3001
+const port = 3002
 
 const dotenv = require('dotenv');
 dotenv.config();
