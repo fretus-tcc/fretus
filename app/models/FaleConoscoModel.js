9edd02657204c4e3ff29cf346baec14d54446fa9
Alex Farias Vaz dos Santos <alexfarias08032006@gmail.com>
2024-09-08 00:36:41 -0300

    puxando por id fale conosco

--- app/models/FaleConoscoModel.js
+++ app/models/FaleConoscoModel.js
@@ -6,15 +6,24 @@ const FaleconoscoModel = {
             await pool.query(
                 'INSERT INTO FaleConosco ( `nome_usuario`, `email_usuario`, `tipo_usuario`, `cpf_usuario`, `assunto`, `mensagem` ) VALUES ( ? , ? , ? , ? , ? , ? ) ', [data.nome, data.email, data.tipo, data.cpf, data.assunto, data.mensagem]
             );
-            
+
         } catch (error) {
             console.log(error)
             throw error;
 
-    
+
         }
     },
-   
+    findByUserId: async (userId) => {
+        try {
+            const [result] = await pool.query('SELECT * FROM FaleConosco WHERE `id_solicitacao` = ?', [userId]);
+            return result; 
+        } catch (error) {
+            throw new Error(`Erro ao buscar o ID ${userId}: ${error.message}`);
+        }
+    },
+    
+
 
 };
 
